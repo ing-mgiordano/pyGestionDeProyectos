3b7da0f9591e99b21bd737fcca180e712c1a4876
ing-mgiordano <ing.mgiordano@gmail.com>
2022-08-27 21:11:41 +0200

    Creamos f() para eliminar y editar tareas

--- backend/controllers/tareaController.js
+++ backend/controllers/tareaController.js
@@ -40,11 +40,52 @@ const obtenerTarea = async (req, res) => {
 }
 
 const actualizarTarea = async (req, res) => {
-    
+    const {id} = req.params
+    const tarea = await Tarea.findById(id).populate("proyecto")
+    /* console.log(tarea) */
+
+    if (!tarea) {
+        const error = new Error("La Tarea no existe")
+        return res.status(404).json({ msg: error.message })
+    }
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("No Tienes Permisos")
+        return res.status(403).json({ msg: error.message })
+    }
+
+    tarea.nombre = req.body.nombre || tarea.nombre
+    tarea.descripcion = req.body.descripcion || tarea.descripcion
+    tarea.prioridad = req.body.prioridad || tarea.prioridad
+    tarea.fechaEntrega = req.body.fechaEntrega || tarea.fechaEntrega
+
+    try {
+        const tareaAlmacenada = await tarea.save()
+        res.json(tareaAlmacenada)
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const eliminarTarea = async (req, res) => {
-    
+    const {id} = req.params
+    const tarea = await Tarea.findById(id).populate("proyecto")
+    /* console.log(tarea) */
+
+    if (!tarea) {
+        const error = new Error("La Tarea no existe")
+        return res.status(404).json({ msg: error.message })
+    }
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error("No Tienes Permisos")
+        return res.status(403).json({ msg: error.message })
+    }
+
+    try {
+        await tarea.deleteOne()
+        res.json({ msg: "Tarea Eliminada Correctamente"})
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 const cambiarEstado = async (req, res) => {
