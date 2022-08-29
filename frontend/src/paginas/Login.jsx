5133dc5b68319ecab30ff63c16a4d68e431d2028
ing-mgiordano <ing.mgiordano@gmail.com>
2022-08-29 17:58:30 +0200

    creamos formulario de autenticacion

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -2,7 +2,48 @@
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+        <h1 className="text-sky-600 font-black text-6xl capitalize">
+            Inicia sesión y administra tus 
+            <span className="text-slate-700"> proyectos</span>
+        </h1>
+
+        <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+            <div className="my-5">
+                <label 
+                    htmlFor="email" 
+                    className="uppercase text-gray-600 block text-xl font-bold cursor-pointer"
+                >Email
+                </label>
+                <input 
+                    type="email"
+                    id="email"
+                    placeholder="Email de Registro"
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                />
+            </div>
+
+            <div className="my-5">
+                <label
+                    htmlFor="password" 
+                    className="uppercase text-gray-600 block text-xl font-bold cursor-pointer"
+                >Password
+                </label>
+                <input 
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+                />
+            </div>
+
+            <input 
+                type="submit" 
+                value="Iniciar Sesión"
+                className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-colors"
+            />
+        </form>
+    </>
   )
 }
 
