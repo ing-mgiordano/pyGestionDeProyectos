dc406a34784540236b4dfbd15e806479ab5e5b30
ing-mgiordano <ing.mgiordano@gmail.com>
2022-08-29 20:12:24 +0200

    se creo pagina de confirmar cuenta y nvo password

--- frontend/src/paginas/ConfirmarCuenta.jsx
+++ frontend/src/paginas/ConfirmarCuenta.jsx
@@ -1,7 +1,12 @@
 
 const ConfirmarCuenta = () => {
   return (
-    <div>Confirmarcuenta</div>
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Confirma tu
+        <span className="text-slate-700"> cuenta</span>
+      </h1>
+    </>
   )
 }
 
--- frontend/src/paginas/NuevoPassword.jsx
+++ frontend/src/paginas/NuevoPassword.jsx
@@ -1,8 +1,50 @@
-
 const NuevoPassword = () => {
   return (
-    <div>nuevopass</div>
-  )
-}
+    <>
+      <h1 className="text-sky-600 font-black text-6xl capitalize">
+        Restablece tu
+        <span className="text-slate-700"> password</span>
+      </h1>
 
-export default NuevoPassword
+      <form action="" className="my-10 bg-white shadow rounded-lg p-10">
+        <div className="my-5">
+          <label
+            htmlFor="password"
+            className="uppercase text-gray-600 block text-xl font-bold cursor-pointer"
+          >
+            Nuevo Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <div className="my-5">
+          <label
+            htmlFor="password2"
+            className="uppercase text-gray-600 block text-xl font-bold cursor-pointer"
+          >
+            Repetir Password
+          </label>
+          <input
+            type="password"
+            id="password2"
+            placeholder="Repetir tu Password"
+            className="w-full mt-3 p-3 border rounded-xl bg-gray-50"
+          />
+        </div>
+
+        <input
+          type="submit"
+          value="Guardar Nuevo Password"
+          className="bg-sky-700 w-full mb-5 py-3 text-white uppercase font-bold rounded-xl hover:cursor-pointer hover:bg-sky-800 transition-colors"
+        />
+      </form>
+    </>
+  );
+};
+
+export default NuevoPassword;
