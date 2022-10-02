0a68cff4259f7886f823a54d59ad74ea278c2b3d
ing-mgiordano <ing.mgiordano@gmail.com>
2022-10-02 19:40:05 +0200

    detalles de diseño

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,7 +7,7 @@ const Sidebar = () => {
     /* console.log(auth) */
 
     return (
-        <aside className="md:w-80 lg:w-96 px-5 py-10 bg-gray-200">
+        <aside className="md:w-1/3 lg:w-1/5 xl:w-1/6 px-5 py-10 bg-gray-200">
             <p className="text-xl font-bold">Hola: {auth.nombre}</p>
 
             <Link
--- frontend/src/components/Tarea.jsx
+++ frontend/src/components/Tarea.jsx
@@ -21,7 +21,7 @@ const Tarea = ({tarea}) => {
                         >Completado por: {tarea.completado.nombre}</p>}
         </div>
 
-        <div className="flex gap-2">
+        <div className="flex flex-col lg:flex-row gap-2">
             {admin && (
                 <button
                     onClick={() => handleModalEditarTarea(tarea)}
